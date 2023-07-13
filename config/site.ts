5763013a0193012d6ed0c97cd384dd43c16eb40f
mask <617473294@qq.com>
2023-07-13 13:49:18 +0800

    feat: index

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+interface SiteConfig {
+  name: string
+  description: string
+  url: string
+  links: { github: string }
+}
+
+// 站点配置
+export const siteConfig: SiteConfig = {
+  name: 'NEXT BOILERPLATE',
+  description:
+    'Next.js boilerplate with TypeScript, ESLint, Prettier, Husky, Lint-Staged, and more.',
+  url: 'https://next.moyaojun.com',
+  links: {
+    github: 'https://github.com/Mask-MJ',
+  },
+}
